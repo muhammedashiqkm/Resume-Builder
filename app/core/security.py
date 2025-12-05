582e5d753c40a6287a6c325f5a32e8a8a0707c32
muhammedashiqkm <ashiq.kottakuthm@gmail.com>
2025-12-05 16:04:45 +0530

    deleted student.json

--- app/core/security.py
+++ app/core/security.py
@@ -13,7 +13,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_HOURS)
+        expire = datetime.now(timezone.utc) + timedelta(hours=settings.ACCESS_TOKEN_EXPIRE_HOURS)
         
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
